f8d0014c959bf2ffb1e98d15e6518d06d80499d9
eduqg <eduardoqgomes@gmail.com>
2017-03-21 22:54:40 -0300

    Inserting TF1 files

--- EP1/EP1/urls.py
+++ EP1/EP1/urls.py
@@ -1,4 +1,4 @@
-"""LivroDeFotos URL Configuration
+"""EP1 URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.10/topics/http/urls/
@@ -13,13 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf import settings
-from django.conf.urls.static import static
 from django.conf.urls import url
 from django.contrib import admin
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ EP1/infoTF1.md
@@ -0,0 +1,16 @@
+Informações EP1
+
+Nome: Eduardo Quintino Gomes
+
+Matrícula: 14/0137068
+
+Nome do projeto: Galeria de Imagens
+
+Endereço no Github:
+Está no arquivo EP1 em https://github.com/eduqg/codesPythonOnDjango
+
+Endereço no PythonAnywhere
+Não estou usando ainda, mas já criei uma conta no site. https://www.pythonanywhere.com/user/eduqg/consoles/
+
+Pequena descrição falando o objetivo do trabalho e se encontrou alguma dificuldade específica (resolvida ou não) durante o desenvolvimento do projeto
+O objetivo principal desse projeto é trabalhar com um CRUD em python on django. Esse CRUD irá conter imagens, o que foi e está sendo um grande desafio para mim.
